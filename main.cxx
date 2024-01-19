6d40445dedbdc6de188f92fe29873dec449a6fec
Stefano Piacentini <stefano.piacentini@roma1.infn.it>
2024-01-19 14:13:24 +0100

    time profiling in main.cxx

--- main.cxx
+++ main.cxx
@@ -3,12 +3,15 @@
 #include "s3.h"
 #include <zlib.h>
 #include <stdexcept>
+#include <chrono>
 
 // Appunti:
 // classe evento
 // immagine solo pixel del cluster + wfs connesse
 
 int main() {
+    
+    bool debug = true;
     
     //open midas file
     std::string filename="/home/piacenst/cygno_workspace/stefano/recopp/debug/run35138.mid.gz";
@@ -24,18 +27,31 @@ int main() {
     
     int counter =0;
     
+    auto start00 = std::chrono::high_resolution_clock::now();
+    
     while (reading) {
         
-        
+        auto start0 = std::chrono::high_resolution_clock::now();
+        std::cout<<"Reading evt "<<counter<<std::endl;
         TMidasEvent event = TMidasEvent();
         reading = TMReadEvent(reader, &event);
+        if (!reading) {
+            // EOF
+            std::cout<<"EOF reached."<<std::endl;
+            break;
+        }
         
         bool cam_found = cygnolib::FindBankByName(event, "CAM0");
         bool dgh_found = cygnolib::FindBankByName(event, "DGH0");
         bool dig_found = cygnolib::FindBankByName(event, "DIG0");
         
+        auto stop0 = std::chrono::high_resolution_clock::now();
+        auto duration0 = std::chrono::duration_cast<std::chrono::milliseconds>(stop0 - start0);
+        if(debug) std::cout<<">> TIME TO READ MIDASEVENT "<< duration0.count()<<" ms"<<std::endl;
+        
         if(dgh_found) {
-            std::cout<<"Reading evt "<<counter<<std::endl;
+            auto start = std::chrono::high_resolution_clock::now();
+            //std::cout<<"Reading evt "<<counter<<std::endl;
             //std::cout<<"DGH0 bank found"<<std::endl;
             cygnolib::DGHeader dgh = cygnolib::daq_dgh2head(event);
             //dgh.Print();
@@ -48,34 +64,49 @@ int main() {
                 std::vector<std::vector<std::vector<uint16_t>>> *fastwfs = pmts.GetWaveforms(1742);
                 std::vector<std::vector<std::vector<uint16_t>>> *slowwfs = pmts.GetWaveforms(1720);
                 
-                std::cout<<"====== fast ====="<<std::endl;
-                int ev = 0;
-                int ch = 1;
-                for(int i =0; i<10; i++) {
-                    std::cout<<(*fastwfs)[ev][ch][i]<<", ";
+                bool print = false;
+                if(print) {
+                    std::cout<<"====== fast ====="<<std::endl;
+                    int ev = 0;
+                    int ch = 1;
+                    for(int i =0; i<10; i++) {
+                        std::cout<<(*fastwfs)[ev][ch][i]<<", ";
+                    }
+                    std::cout<<std::endl;
+                    std::cout<<"====== slow ====="<<std::endl;
+                    for(int i =0; i<10; i++) {
+                        std::cout<<(*slowwfs)[ev][ch][i]<<", ";
+                    }
+                    std::cout<<std::endl;
                 }
-                std::cout<<std::endl;
-                std::cout<<"====== slow ====="<<std::endl;
-                for(int i =0; i<10; i++) {
-                    std::cout<<(*slowwfs)[ev][ch][i]<<", ";
-                }
-                std::cout<<std::endl;
                 
             }
             
+            auto stop = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+            if(debug) std::cout<<">> TIME TO INIT DGH0 AND DIG0 "<< duration.count()<<" ms"<<std::endl;
         }
+        
         if(cam_found) {
+            auto start = std::chrono::high_resolution_clock::now();
             cygnolib::Picture pic=cygnolib::daq_cam2pic(event, "fusion");
             //pic.Print(4,4); // print upper left 4x4 angle
             //pic.SavePng("/data11/cygno/piacenst/stefano/cygnocpp/debug/test.png");
             
-            counter ++;
+            auto stop = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+            if(debug) std::cout<<">> TIME TO INIT CAM0 "<< duration.count()<<" ms"<<std::endl;
         }
         
+        counter ++;
         
     }
+    
     reader->Close();
     delete reader;
+    auto stop00 = std::chrono::high_resolution_clock::now();
+    auto duration00 = std::chrono::duration_cast<std::chrono::milliseconds>(stop00 - start00);
+    if(debug) std::cout<<">> TIME TO READ ALL MIDAS FILE "<< duration00.count()<<" ms"<<std::endl;
     
     return 0;
 }
